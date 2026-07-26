91eb0240e57eac11463d1b7c4298fa51cc4a7cd4
Stella Ship <stacktellertrustbank.support@gmail.com>
2026-07-26 11:33:39 +0100

    Update base.py
    Save

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -163,7 +163,7 @@ EMAIL_HOST = smtp-relay.brevo.com
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-
+EMAIL_TIMEOUT = 10  # seconds
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
