e9006a9d733b64db4c5c8bc621c88cdc1d3a751d
Stella Ship <stacktellertrustbank.support@gmail.com>
2026-07-26 12:59:25 +0100

    Update base.py
    Save

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -160,9 +160,9 @@ CORS_ALLOW_CREDENTIALS = True
 # Email Configuration (SMTP - Brevo)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp-relay.brevo.com"
-EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_TIMEOUT = 10  # seconds
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
